0eaa91d43a3e7de3b61502713062392ad1d5219d
Tobias Wallura <tobias@wallura.org>
2018-08-15 16:05:55 +0200

    setup ask sdk project with typescript and webpack

---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import * as path from "path";
+
+const config = {
+  entry: "./src/index.ts",
+  externals: ["aws-sdk"],
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: "raw-loader",
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  output: {
+    filename: "index.js",
+    library: "handler",
+    libraryTarget: "commonjs2",
+    path: path.resolve(__dirname, "dist"),
+  },
+  performance: {
+    hints: false,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
+
+export default config;
